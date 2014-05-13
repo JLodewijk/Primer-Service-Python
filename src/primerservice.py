d7826d73e118792fdf8c312ac185fb35bb648cbc
mhroelfes <mhroelfes@bin256.bin>
2014-05-13 15:43:10 +0200

    added input Checker

--- src/primerservice.py
+++ src/primerservice.py
@@ -7,17 +7,32 @@ __date__ ="$Apr 23, 2014 11:07:32 AM$"
 import sys
 import cherrypy
 
-class HelloWorld(object):
-    def index(self):
-        return "Hello World!"
-    index.exposed = True
+class PrimerService():
+    def __init__(self):
+        try:
+            self.checkInputFile("example.fasta")
+        except Exception as e:
+            print(e)
 
-cherrypy.quickstart(HelloWorld())
 
-#class PrimerService():
-#    def __init__(self):
-#        print("main program")
-#        print(sys.path)
-#
-#if __name__ == "__main__":
-#    PrimerService()
+
+
+
+    def checkInputFile(self, file):
+        """
+            Author: Marco Roelfes
+            Checks if uploaded file is in the right format and only contain one sequences
+
+        """
+
+        file = file.split(".")
+        if file[-1] != "fasta" and file[-1] != "fa" and file[-1] != "gb":
+            raise Exception("Uploaded file has to be in FASTA or GeneBank format")
+        
+
+
+
+
+
+if __name__ == "__main__":
+    PrimerService()
